8021edf468c4f09f93bc2c77c329fa3d51717f0e
neal.nankivell <neal.nankivell@gmail.com>
2019-09-07 13:18:53 +0200

    Implmented solution to reverse a linked list
    Performing a single pass over the list we can keep track of the last
    seen item and flip the next references as we go.

--- solution.py
+++ solution.py
@@ -1,3 +1,26 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        return "%s->%s" % (self.val, self.next)
+
+
 class Solution:
-    def foo(self) -> str:
-        return "bar"
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+
+        previous = head
+        current = previous.next
+        previous.next = None
+
+        while current:
+            temp = current.next
+            current.next = previous
+            previous = current
+            current = temp
+
+        return previous
--- test_solution.py
+++ test_solution.py
@@ -1,13 +1,37 @@
 import solution
 import unittest
+from solution import ListNode
+from typing import List
 
 
 class Test_TestSolution(unittest.TestCase):
     def setUp(self):
         self.sut = solution.Solution()
 
-    def test_foo(self):
-        self.assertEqual(self.sut.foo(), "bar")
+    def test_reverseList(self):
+        result = self.create_list(
+            self.sut.reverseList(self.create_linked_list([1, 2])))
+
+        self.assertEqual(result, [2, 1])
+
+    def create_linked_list(self, list: List[int]) -> ListNode:
+        head = ListNode(list[0])
+
+        last = head
+        for num in list[1:]:
+            last.next = ListNode(num)
+            last = last.next
+
+        return head
+
+    def create_list(self, head: ListNode) -> List[int]:
+        list = []
+        current = head
+        while current:
+            list.append(current.val)
+            current = current.next
+
+        return list
 
 
 if __name__ == '__main__':
